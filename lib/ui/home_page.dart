f28bba981e14b87636f6b6dd4f8ca8e1d4d9d430
rrifafauzikomara <rifafauzi6@gmail.com>
2023-12-01 17:49:29 +0700

    extract widget

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -1,9 +1,9 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_ml_kit/bloc/bloc.dart';
 import 'package:flutter_ml_kit/utils/view_data.dart';
+import 'package:flutter_ml_kit/widget/field_item.dart';
+import 'package:flutter_ml_kit/widget/image_view.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -12,33 +12,6 @@ class HomePage extends StatelessWidget {
 
   void _openGallery(BuildContext context) {
     context.read<ScanBloc>().add(ScanIdCard());
-  }
-
-  Widget _field({required String title, required String value}) {
-    return Container(
-      margin: const EdgeInsets.only(bottom: 5),
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          SizedBox(
-            width: 100,
-            child: Text(title),
-          ),
-          Container(
-            margin: const EdgeInsets.symmetric(horizontal: 10),
-            child: const Text(':'),
-          ),
-          Expanded(child: Text(value)),
-        ],
-      ),
-    );
-  }
-
-  Widget _image({required File file}) {
-    return SizedBox(
-      height: 250,
-      child: Image.file(file),
-    );
   }
 
   @override
@@ -63,21 +36,27 @@ class HomePage extends StatelessWidget {
                   mainAxisSize: MainAxisSize.min,
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    _image(file: data.filePhoto),
+                    ImageView(file: data.filePhoto),
                     const SizedBox(height: 30),
-                    _field(title: 'ID', value: data.cardModel.id),
-                    _field(title: 'Name', value: data.cardModel.name),
-                    _field(title: 'Date of Birth', value: data.cardModel.dob),
-                    _field(
+                    FieldItem(title: 'ID', value: data.cardModel.id),
+                    FieldItem(title: 'Name', value: data.cardModel.name),
+                    FieldItem(
+                      title: 'Date of Birth',
+                      value: data.cardModel.dob,
+                    ),
+                    FieldItem(
                       title: 'Place of Birth',
                       value: data.cardModel.placeOfDob,
                     ),
-                    _field(title: 'Gender', value: data.cardModel.gender),
-                    _field(title: 'Address', value: data.cardModel.address),
-                    _field(title: 'Religion', value: data.cardModel.religion),
-                    _field(title: 'Status', value: data.cardModel.status),
-                    _field(title: 'Job', value: data.cardModel.job),
-                    _field(
+                    FieldItem(title: 'Gender', value: data.cardModel.gender),
+                    FieldItem(title: 'Address', value: data.cardModel.address),
+                    FieldItem(
+                      title: 'Religion',
+                      value: data.cardModel.religion,
+                    ),
+                    FieldItem(title: 'Status', value: data.cardModel.status),
+                    FieldItem(title: 'Job', value: data.cardModel.job),
+                    FieldItem(
                       title: 'Nationality',
                       value: data.cardModel.nationality,
                     ),
